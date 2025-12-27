9837f3a3ca185b0340c864d843f682e6ce841342
Julia Federova Sitio <juliasitio1274@gmail.com>
2025-12-27 07:03:33 +0700

    feat: finalize preprocessing and integrate automate_Julia.py

--- MLProject/modelling_tuning.py
+++ MLProject/modelling_tuning.py
@@ -1,16 +1,21 @@
+import sys
+import os
+# Menambahkan folder utama ke jalur pencarian Python
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import pandas as pd
 import mlflow
 import mlflow.sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import os
+from automate_Julia import preprocess_data
 
 mlflow.set_experiment("Final_Submission_Julia")
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 path_data = os.path.join(base_dir, "namadataset_preprocessing", "occupancy_processed.csv")
 
-df = pd.read_csv(path_data)
+df = preprocess_data(path_data)
 X = df.drop(columns=['Room_Occupancy_Count'])
 y = df['Room_Occupancy_Count']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
