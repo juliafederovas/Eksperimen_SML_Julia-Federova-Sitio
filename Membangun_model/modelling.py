f65793e3e48d733671741f497ab85833e3ca1d90
Julia Federova Sitio <juliasitio1274@gmail.com>
2025-12-30 18:43:35 +0700

    Finish modelling and tuning

--- Membangun_model/modelling.py
+++ Membangun_model/modelling.py
@@ -6,16 +6,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, f1_score
 import os
-
-# 1. Hubungkan ke DagsHub 
+ 
 dagshub.init(repo_owner='juliafederovas', repo_name='Eksperimen_SML_Julia-Federova-Sitio', mlflow=True)
 
-# 2. Set Eksperimen 
+mlflow.autolog()
+
 mlflow.set_experiment("Occupancy_Estimation_Baseline")
 base_dir = os.path.dirname(os.path.abspath(__file__))
 path_data = os.path.join(base_dir, "namadataset_preprocessing", "occupancy_processed.csv")
-
-# 3. Load Data
+ 
 if not os.path.exists(path_data):
     raise FileNotFoundError(f"File dataset tidak ditemukan. Pastikan folder 'namadataset_preprocessing' sudah ada di folder 'Membangun_model'!")
 
@@ -23,22 +22,14 @@ df = pd.read_csv(path_data)
 X = df.drop(columns=['Room_Occupancy_Count'])
 y = df['Room_Occupancy_Count']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# 4. Baseline Training 
+ 
 with mlflow.start_run(run_name="RandomForest_Baseline_Julia"):
     model = RandomForestClassifier(random_state=42)
     model.fit(X_train, y_train)
     
     y_pred = model.predict(X_test)
+
+    # acc = accuracy_score(y_test, y_pred)
+    #mlflow.log_metric("accuracy", acc)
     
-    # Log Metrics
-    acc = accuracy_score(y_test, y_pred)
-    f1 = f1_score(y_test, y_pred, average='weighted')
-    
-    mlflow.log_metric("accuracy", acc)
-    mlflow.log_metric("f1_score", f1)
-    
-    # Log Model 
-    mlflow.sklearn.log_model(model, "model_baseline")
-    
-    print(f"Model Baseline Selesai. Akurasi: {acc}")
+    print("Model Baseline Selesai dengan Autologging.")
